371c1e1abd80948352a59932009914fa67e86858
Charles Nicholson <cnicholson12@student.monroecc.edu>
2014-11-28 13:13:57 -0500

    Began work on Lab6 tester file.

--- Bookstore Local/Lab6.cpp
+++ Bookstore Local/Lab6.cpp
@@ -2,10 +2,18 @@
 #include <fstream>
 #include <ostream>
 #include <string>
+#include <iomanip>
 #include "Book.h"
 #include "Bookstore.h"
 
+using namespace std;
 
+//*****************************************************************************
+// Method Function: Ensures the file can be opened and read safely.
+// Preconditions: The ifstream has been opened.
+// Postconditions: None
+//*****************************************************************************
+void OpenFile(ifstream &in_File);
 
 int main()
 {
